7704949abec748ce4bdca29a309da9aa3bf76745
asif93-138 <asif93@student.sust.edu>
2024-03-22 23:07:31 +0600

    update bug fixed on ins dashboards!

--- src/MyClasses.jsx
+++ src/MyClasses.jsx
@@ -4,11 +4,33 @@ import { CampContext } from '../ContextProvider';
 const MyClasses = () => {
     const {user} = useContext(CampContext);
     const [clsDt, setClsDt] = useState([]);
+    const [count, setCount] = useState(0);
     useEffect(() => {
         fetch(`http://localhost:3000/classes/${user?.uid}`)
         .then(res => res.json())
         .then(data => setClsDt(data))
-    }, [user])
+    }, [user, count])
+    function updateCourse(event) {
+        event.preventDefault();
+        const form = event.target;
+        const updatedObj  = {
+            seats : form.seats.value, price: form.price.value
+        };
+        
+        fetch(`http://localhost:3000/updatethecourse/${form.id.value}`, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(updatedObj)
+        })
+        .then(res => res.json())
+        .then(data => {
+            if (data.acknowledged) {
+                alert('data updated!'); setCount(count + 1); form.reset();
+            }
+        })
+    }
     return (
         <div>
            <p>my classes</p>
@@ -20,6 +42,12 @@ const MyClasses = () => {
             <p>Enrolled : {x.enrolled}</p>
             <p>Status : {x.cStatus}</p>
             {x?.adminFB && <p>Feedback : {x?.adminFB}</p>}
+            <form onSubmit={updateCourse}>
+                <input type='number' name='seats' placeholder='update available seats' />
+                <input type='number' name='price' placeholder='update price' />
+                <input type='hidden' name='id' value={x._id} />
+                <button type='submit'>Update</button>
+            </form>
            </div>))}
         </div>
     );
